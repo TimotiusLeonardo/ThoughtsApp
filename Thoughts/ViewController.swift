6217ae8392daa000a73adf5ed828589058d2fb56
TimotiusLeonardo <lionato14@gmail.com>
2021-08-04 11:06:21 +0700

    Initial Commit

---
+++ Thoughts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thoughts
+//
+//  Created by Timotius Leonardo Lianoto on 04/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
